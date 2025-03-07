b9bc0f06bb0b038730d61209929c987100eee98f
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2025-03-07 06:27:41 -0800

    Add files via upload

---
+++ data distribution.py
@@ -0,0 +1,15 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.uniform(0.0, 5.0, 250)
+
+plt.hist(x, 5)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()

645bd718568d8d9ce84095a9f8b876aa6af9c9ad
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2025-03-10 08:19:55 -0700

    Add files via upload

---
+++ largest common prefix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+       
+        prefix = strs[0]
+        
+        for s in strs[1:]:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
+
+
+sol = Solution()
+print(sol.longestCommonPrefix(["flower", "flow", "flight"]))  
+print(sol.longestCommonPrefix(["dog", "racecar", "car"]))      
